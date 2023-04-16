c618cff5d3492b04de525133dbea4bda06a39cd8
H8M33 <90274360+H8M33@users.noreply.github.com>
2023-04-16 17:19:47 +0300

    Update DataMiningCW.py

--- DataMiningCW.py
+++ DataMiningCW.py
@@ -1,7 +1,11 @@
 import numpy as np
+
 deep, matrix_range = [int(i) for i in input().split()]
+# deep - количество итераций, "глубина" поиска. matrix_range - количество вершин в графе
 mat_in = np.array([list(map(int,input().split())) for i in range(matrix_range)], list)
+# вводим матрицу, описывающую граф
 mat_out = np.transpose(mat_in)
+# Получаем транспонированную матрицу
 h = np.ones((matrix_range,1))
 a = []
 for i in range(deep):
@@ -9,6 +13,7 @@ for i in range(deep):
     a = mat/mat.max()
     mat = np.dot(mat_in, a)
     h = mat/mat.max()
+# Применяем HITS, выводим значения метрик
 print("Метрика A:")
 print(a)
 print("Метрика H:")
